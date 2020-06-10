a8eb49eda9810afaf9f2c987828fee981c2d5da8
Seunghwan Kim <200359764@student.georgianc.on.ca>
2020-06-10 18:26:17 -0400

    add contact ejs for lab3

--- routes/pages.js
+++ routes/pages.js
@@ -1,9 +1,11 @@
 console.log("exported the module");
-const { home, about} = require('../controllers/PagesController');
+const { home, about, contact } = require('../controllers/PagesController');
 
 module.exports = router => {
     router.get('/', home);
 
 
     router.get('/about', about);
+
+    router.get('/path', contact);
 };
