4950f3f1feab734d9ed2567fae353e5eb1b304cf
Seunghwan Kim <200359764@student.georgianc.on.ca>
2020-06-18 00:13:25 -0400

    Lesson4

--- routes.js
+++ routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 
 (require('./routes/pages'))(router);
+(require('./routes/blogs'))(router);
 
 module.exports = router;
--- routes/pages.js
+++ routes/pages.js
@@ -1,9 +1,7 @@
-console.log("exported the module");
 const { home, about, contact } = require('../controllers/PagesController');
 
 module.exports = router => {
     router.get('/', home);
-
 
     router.get('/about', about);
 
