7881a65646da8710ce37e48c6a9b787f5194494b
Seunghwan Kim <200359764@student.georgianc.on.ca>
2020-06-25 14:20:34 -0400

    finished CRUD today

--- models/blog.js
+++ models/blog.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 
 const BlogSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
+  
   title: {
     type: String,
     required: true // This must exist
